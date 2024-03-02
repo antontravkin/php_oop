769f9371c2e44021891f7cbae77bbe0e77177856
Anton Travkin <travkin.rh@gmail.com>
2024-03-02 23:04:08 +0300

    test

--- lasson_1/2-animals.php
+++ lasson_1/2-animals.php
@@ -1,7 +1,4 @@
 <?php 
-    class Animals{
-        public $animal;
-    }
 
     class Cat{
         public $name;
@@ -10,11 +7,28 @@
         public $alive;
 
         public function __construct( string $name, int $health, int $power){
-            this->name = $name;
-            this->health = $health;
-            this->power = $power;
-            this->alive = true;
+            this->name = $name; //имя
+            this->health = $health; //здоровье
+            this->power = $power; //сила
+            this->alive = true; //жив
 
         }
+
+        public function calcDamage(){ // урон
+            return $this->power * (mt_rand(1, 3) / 0.5);
+        }
+
+        public function getDamage (int $damage){
+            $this->health -= $damage;
+        }
     }
+
+    $barsik = new Cat('barsik', 100, 10);
+
+    $barsik->calcDamage();
+
+    echo '<pre>';
+    print_r( $barsik );
+    echo '<pre>';
+
 ?>
