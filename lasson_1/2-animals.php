9b6e506e08d4a7037471e5594c6cac742cdac62d
Anton Travkin <travkin.rh@gmail.com>
2024-03-02 22:48:17 +0300

    Add 2-animals.php

--- lasson_1/2-animals.php
+++ lasson_1/2-animals.php
@@ -8,5 +8,13 @@
         public $health;
         public $power;
         public $alive;
+
+        public function __construct( string $name, int $health, int $power){
+            this->name = $name;
+            this->health = $health;
+            this->power = $power;
+            this->alive = true;
+
+        }
     }
 ?>
