459dfef9de1cac44270ef3b8fc7c6160d3adb9f8
Anton Travkin <travkin.rh@gmail.com>
2024-03-14 03:07:34 +0300

    test

---
+++ lesson_1/main.js
@@ -0,0 +1,32 @@
+let select = function () {
+    let selectHeader = document.querySelectorAll('.select__header')
+    let selectItem = document.querySelectorAll('.select__item')
+    let option = document.querySelectorAll('option')
+
+    selectHeader.forEach((item) => {
+        item.addEventListener('click', selectToggle)
+    })
+    selectItem.forEach((item) => {
+        item.addEventListener('click', selectChoose)
+    })
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active')
+    }
+
+    function selectChoose() {
+        let text = this.innerText,
+            select = this.closest('.select'),
+            currntText = select.querySelector('.select__current')
+        currntText.innerText = text
+        select.classList.remove('is-active')
+
+        option.forEach((item) => {
+            console.log()
+
+            if (item.value == this.getAttribute('data-test')) {
+                item.setAttribute('selected', true)
+            }
+        })
+    }
+}
+select()
